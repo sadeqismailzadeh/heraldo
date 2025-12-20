a6a6c35232004c0b5685c9d189ada8f63a345998
Sadeq Ismailzadeh <sadeqismailzadeh@gmail.com>
2025-12-21 00:39:43 +0330

    fix setup

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="quantum_agent",
     version="0.1",
-    package_dir={"": "quantum_agent"},
-    packages=find_packages(where="quantum_agent"),
+    packages=[p for p in find_packages() if p.startswith("quantum_agent")],
     install_requires=[
         "gymnasium",
         "stable-baselines3",
