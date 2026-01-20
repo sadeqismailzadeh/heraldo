988df70a88317f093de71cd074258a4224f239da
Sadeq Ismailzadeh <sadeqismailzadeh@gmail.com>
2026-01-20 21:42:17 +0330

    init applies patches

--- quantum_agent/optimization/__init__.py
+++ quantum_agent/optimization/__init__.py
@@ -1,5 +1,17 @@
 """
 Optimization module for deterministic quantum circuit parameter search.
 """
-from quantum_agent.optimization.runner import OptimizationRunner
-from quantum_agent.optimization.gaussian_runner import GaussianOptimizationRunner
+
+# disable caching to save memory for large cutoff dims
+from quantum_agent.patches.sf_operations_no_cache import disable_fock_caching
+disable_fock_caching()
+
+# optimized loss channel
+from quantum_agent.patches.monitored_loss_measure_fock_patch import MonitoredLossMeasureFock, patch_monitored_loss_measure_fock, decode_measurement_result
+patch_monitored_loss_measure_fock()
+
+from quantum_agent.patches.beamsplitter_patch import patch_beamsplitter
+patch_beamsplitter()
+
+from quantum_agent.patches.prepare_multimode_patch import patch_prepare_multimode
+patch_prepare_multimode()
